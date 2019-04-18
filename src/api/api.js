8808f65544a88223b1472a529b58c975ff6a4a4e
jianghaoran <jianghaoran.com>
2019-04-18 11:10:08 +0800

    接口

--- src/api/api.js
+++ src/api/api.js
@@ -12,23 +12,23 @@ export const getUserInfoByUserOpenIdWithoutToken = params => {
 
 /************** 首页一级分类*/
 export const selectCatalogParentWithoutToken= params => {
-    return axios.post("api/biz/v01/selectTopCatalogWithoutToken.do",params); 
+    return axios.post("/api/biz/v01/selectTopCatalogWithoutToken.do",params); 
 };
 /**********搜索商品 */
 export const seachShop =params =>{
-    return axios.post('api/biz/v01/listBizDrug.do',params);
+    return axios.post('/api/biz/v01/listBizDrug.do',params);
 }
 /*********添加到购物车 */
 export const addShop =params =>{
-    return axios.post('api/biz/v01/saveBizUserTrolleyWithoutToken.do',params);
+    return axios.post('/api/biz/v01/saveBizUserTrolleyWithoutToken.do',params);
 }
 /**********搜索所有商品 */
 export const loadingShop =params =>{
-    return axios.post('api/biz/v01/listBizProduct.do',params);
+    return axios.post('/api/biz/v01/listBizProduct.do',params);
 }
 /***********商品模块 */
 export const shopmodel =params =>{
-    return axios.post('api/biz/v01/selectProductByCatalogId.do',params);
+    return axios.post('/api/biz/v01/selectProductByCatalogId.do',params);
 }
 /************积分商城 */
 export const integral =params =>{
