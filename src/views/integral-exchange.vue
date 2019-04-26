780984f885585aeeeb5df19daa8dd2983ed6e821
jianghaoran <jianghaoran.com>
2019-04-26 10:14:36 +0800

    商品详情bug修改

--- src/views/integral-exchange.vue
+++ src/views/integral-exchange.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-    <header-general  headTitle="兑换记录" headClass="style4" routerTo='/'></header-general>
+    <header-general  headTitle="兑换记录" headClass="style6" routerTo='/'></header-general>
     <div class="integralbox">
         <div class="integralmain">
             <div class="integralmessage">
